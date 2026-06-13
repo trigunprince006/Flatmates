87bfb3ca2b65ba955193e50d55f8c91e1dfc7827
trigunprince006 <princetrigun0@gmail.com>
2026-06-13 13:23:56 +0530

    Fixed the issue in refreshToken generator

--- backend/controllers/broker/refreshToken.controller.js
+++ backend/controllers/broker/refreshToken.controller.js
@@ -1,5 +1,4 @@
-const brokerModel = require("../../models/broker.model");
-const tempUserModel = require("../../models/otp.Model");
+const brokerModel = require('../../models/broker.model')
 const jwt = require("jsonwebtoken");
 
 async function refreshToken(req, res) {
@@ -11,17 +10,21 @@ async function refreshToken(req, res) {
     });
   }
   const decoded = jwt.verify(refreshToken, process.env.REFRESH_JWT_SECRET_KEY);
+  // console.log(decoded)
   const brokerId = decoded.brokerId
-  const isBroker = await brokerModel.findOne({brokerId});
-  console.log("isBroker:",isBroker)
+  // console.log(brokerId);
+
+  const isBroker = await brokerModel.findById(brokerId);
+
+  // console.log("isBroker:",isBroker)
   if (!isBroker) {
     return res.status(400).json({
-      message: "Invalid refresh Token",
+      message: "Invalid refresh Token,Broker does't not exist",
     });
   }
   if (isBroker.refreshToken !== refreshToken) {
   return res.status(401).json({
-    message: "Invalid refresh token",
+    message: "Refresh token is not matching with our server refresh token",
   });
 }
   if (decoded && isBroker) {
