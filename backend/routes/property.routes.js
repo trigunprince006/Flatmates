3a2bcdbf04da720415f5a468af66c2936bd2343b
trigunprince006 <princetrigun0@gmail.com>
2026-06-15 11:46:44 +0530

    Added a getProperties route for fetching all properties

--- backend/routes/property.routes.js
+++ backend/routes/property.routes.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
-
+//Importing controllers
 const propertyController = require('../controllers/broker/createProperty.controller')
 const authMiddleware = require('../middleware/auth')
 const upload = require('../middleware/multer')
+const getAllPropertyController = require('../controllers/user/properties.controller')
 
+//Rouetes
 router.post('/list-property',authMiddleware,upload.array('images',20),propertyController);
+
+router.get('/property',getAllPropertyController)
+
 
 module.exports = router;
