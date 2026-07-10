58ce53ae798eecf50c19dd043002611a0e15380a
trigunprince006 <princetrigun0@gmail.com>
2026-07-10 16:44:21 +0530

    added a "success:false" in response

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -7,6 +7,7 @@ async function auth(req, res, next) {
 
     if (!accessToken) {
       return res.status(401).json({
+        success:false,
         message: "Please login first",
       });
     }
@@ -17,11 +18,11 @@ async function auth(req, res, next) {
     );
 
     req.user = decoded;
-    // console.log("Decoded",decoded)
-    // console.log("req.user",req.user)
+
     next();
   } catch (error) {
     return res.status(401).json({
+      success:false,
       message: "Invalid or expired access token",
     });
   }
