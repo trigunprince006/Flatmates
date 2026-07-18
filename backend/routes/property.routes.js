d566fe870cbfaeed2e6478e584d7457fb4e33fc7
trigunprince006 <princetrigun0@gmail.com>
2026-07-18 11:07:12 +0530

    added a two api , editProperty & deleteProperty

--- backend/routes/property.routes.js
+++ backend/routes/property.routes.js
@@ -5,11 +5,14 @@ const router = express.Router();
 const propertyController = require('../controllers/broker/createProperty.controller')
 const authMiddleware = require('../middleware/auth')
 const upload = require('../middleware/multer')
-const {getAllProperty,getPropertyById} = require('../controllers/user/properties.controller')
+const {getAllProperty,getPropertyById} = require('../controllers/user/properties.controller');
+const editProperty = require('../controllers/broker/editProperty.controller');
+const deleteProperty = require('../controllers/broker/deletProperty.controller');
 
 // Routes
 router.post('/list-property',authMiddleware,upload.array('images',20),propertyController);
-
+router.put('/edit-property',authMiddleware,upload.array('images',20),editProperty);
+router.delete('/delete-property',authMiddleware,deleteProperty);
 router.get('/property',getAllProperty)
 
 router.get("/house/id",getPropertyById);
